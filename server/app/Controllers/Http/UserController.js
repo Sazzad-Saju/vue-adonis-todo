f56eb5133f2b91355329886769bd665b39dcb926
Sazzad-Saju <sazzadsaju17@gmail.com>
2022-02-09 10:49:08 +0600

    registration_login

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -1,18 +1,25 @@
 'use strict'
 const User = use('App/Models/User');
 class UserController {
+    async login({ request, auth }) {
+        const { email, password } = request.all();
+        const token = await auth.attempt(email, password);
+        return token;
+    }
+
     async register({ request }) {
         const { email, password } = request.all();
-        const user = await User.create({
-                email,
-                password,
-                username: email,
-            })
-            // console.log(email, password)
-            // return {
-            //     message: 'hello world',
-            // };
-        return user;
+        // const user = await User.create({
+        await User.create({
+            email,
+            password,
+            username: email,
+        });
+        // console.log(email, password)
+        // return {
+        //     message: 'hello world',
+        // };
+        return this.login(...arguments);
     }
 }
 
--- server/start/routes.js
+++ server/start/routes.js
@@ -30,5 +30,6 @@ const Route = use('Route')
 
 Route.group(() => {
         Route.post('auth/register', 'UserController.register');
+        Route.post('auth/login', 'UserController.login');
     })
     .prefix('api');
